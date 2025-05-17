530752540140b05a8a81abd8880dd48afcd8c32e
Karan Aryal <karanaryal@Karans-MacBook-Air.local>
2025-05-17 16:01:43 -0600

    update docstring and comment the code

--- insertion-sort/main.py
+++ insertion-sort/main.py
@@ -1,17 +1,37 @@
 def insertion_sort(sample):
+    """
+    Use Insertion Sort algorithm and sort in
+    monotonically decreasing order.
 
+    :param sample List of numbers.
+
+    Returns: None
+    """
+
+    # Iterate over a list of numbers
     for number in range(1, len(sample)):
+
+        # Get the current item
         element = sample[number]
+
+        # Get the previous item
         position = number - 1
+
+        # Compare between the two and shift small value
+        # to the right
         while position >= 0 and sample[position] < element:
             sample[position + 1] = sample[position]
+
+            # Move to the next item
             position = position - 1
 
         sample[position + 1] = element
+
+    # Return the sorted list.
     return sample
 
-def main():
 
+def main():
     sample1 = [15, 43, 2, 95, 37, 8, 49]
     print(f"Sample1: {sample1}")
     print(f"Result: {insertion_sort(sample1)}\n")
